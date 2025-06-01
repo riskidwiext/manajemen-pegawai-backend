aa874baf993251d0c83731b0f7c6d5c5a0fea026
home <riskidwipatrio.ext@gmail.com>
2025-06-01 22:17:00 +0700

    remove

--- package/minio.js
+++ package/minio.js
@@ -1,10 +1,13 @@
 const Minio = require('minio');
 
 const minioClient = new Minio.Client({
-  endPoint: 'cdnapi.riski-labs.site', 
-  useSSL: true,
-  accessKey: 'minio',    
-  secretKey: 'Poweranger@90', 
+  endPoint: process.env.MINIO_ENDPOINT,
+  port: process.env.MINIO_PORT ? parseInt(process.env.MINIO_PORT) : undefined,
+  useSSL: process.env.MINIO_USE_SSL === 'true',
+  accessKey: process.env.MINIO_ACCESS_KEY,
+  secretKey: process.env.MINIO_SECRET_KEY,
 });
+
+console.log(process.env.MINIO_ENDPOINT)
 
 module.exports = minioClient;
--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const dotenv = require('dotenv');
+const dotenv = require('dotenv').config();
 const userRoutes = require('./routes/user');
 const fileManagerRoutes = require('./routes/fileManager')
 const cors = require('cors')
 const pegawaiRouter = require('./routes/pegawai');
 
-dotenv.config();
 const app = express();
 app.use(cors())
 app.use(express.json());
@@ -14,7 +13,7 @@ app.use('/api/users', userRoutes);
 app.use('/api/files', fileManagerRoutes);
 app.use('/api/pegawai', pegawaiRouter);
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         console.log('MongoDB connected');
         app.listen(process.env.PORT, () => {
